c9f82f8b3c39f2720af33168a1fb11b65401e654
Zachary Bethem <zbethem@outlook.com>
2023-11-01 07:21:18 -0700

    correction for end-of-month

--- TimesheetTrigger/timesheet.py
+++ TimesheetTrigger/timesheet.py
@@ -47,9 +47,9 @@ def get_workperiod(end: str) -> dict:
         end_dt = date(end_dt.year, end_dt.month, 15)
         log.info(f"Using first period for month: {end_dt.month}")
     else:
-        start_dt = date(end_dt.year, end_dt.month, 16)
         last_day = end_dt + timedelta(days=-1)
         end_dt = last_day
+        start_dt = date(last_day.year, last_day.month, 16)
         log.info(f"Using second period for month: {end_dt.month}")
 
     return { "start_date": start_dt, "end_date": end_dt}
